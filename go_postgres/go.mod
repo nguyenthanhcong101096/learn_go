a3fe6bb2811e8944a4506e4e7106ee25ca77b7ec
nguyenthanhcong101096 <nguyenthanhcong101096@gmail.com>
2021-03-30 22:53:57 +0700

    update golang learn

--- go_postgres/go.mod
+++
@@ -1,5 +0,0 @@
-module go_postgres
-
-go 1.14
-
-require github.com/lib/pq v1.7.0
--- go_postgres/repository/user_repo.go
+++
@@ -1,9 +0,0 @@
-package repository
-
-import (
-	models "go_postgres/model"
-)
-
-type UserRepo interface {
-	Select() ([]models.User, error)
-}
